51a8f1eae36c9294ac5415d5aa24dd3fc928a52c
mosa-150505 <mosa.raharimanou@gmail.com>
2026-09-24 21:56:18 +0300

    Sahala initialization

---
+++ plagiarism_detector.py
@@ -0,0 +1,8 @@
+from difflib import SequenceMatcher
+
+with open('content.txt') as one_file, open('content2.txt') as two_file:
+    data_file1 = one_file.read()
+    data_file2 = two_file.read()
+    matches = SequenceMatcher(None, data_file1, data_file2).ratio()
+
+    print(f"The plagiarized content is {matches * 100}%")
---
+++ readme.md
@@ -0,0 +1,7 @@
+----  This project is only for fun  -----
+
+--  activate the venv before launching the progam
+        source venv/bin/activate.fish  (for fish terminal)
+        source venv/bin/activate  (for default terminal)
+
+--  run the project by : python plagiarism_detector.py
